551b96f533e1a48ebcb55f44b1e1e7039cfef2c7
Josh Thompson <thompsonjoshd@gmail.com>
2017-08-28 14:43:08 -0600

    finish pangram

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -2,20 +2,26 @@ require 'pry'
 class Pangram
 
   def self.pangram?(phrase)
-    binding.pry
-    current = phrase.split('').uniq.join.gsub('/[^a-z]/i', '')
-    p current
-    p current.length
-    evaluate_result(current)
+    results = format_phrase(phrase)
+    evaluate_result(results)
   end
 
   private
-
-    def self.evaluate_result(phrase)
-      if phrase.length == 26
-         true
-      else
-        false
+    def self.format_phrase(input)
+      input.downcase.split('').uniq.reject do |c|
+        c.match(/[^a-z]/i)
       end
     end
+
+    def self.evaluate_result(array)
+      ('a'...'z').to_a.each do |char|
+        return false unless array.include?(char)
+      end
+      true
+    end
+end
+
+
+class BookKeeping
+  VERSION = 3
 end
