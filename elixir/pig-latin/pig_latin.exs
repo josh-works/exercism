d23a41936f5df393f1f521fe936edadc5ede393b
Josh Thompson <thompsonjoshd@gmail.com>
2019-01-13 13:05:54 -0700

    elixir pig latin first pass

--- elixir/pig-latin/pig_latin.exs
+++ elixir/pig-latin/pig_latin.exs
@@ -15,5 +15,27 @@ defmodule PigLatin do
   """
   @spec translate(phrase :: String.t()) :: String.t()
   def translate(phrase) do
+    phrase
+    |> String.split(" ") 
+    |> Enum.reduce([], fn x, acc -> acc ++ [translate_single_word(x)] end)
+    |> Enum.join(" ")
+  end
+  
+  def translate_single_word(phrase) do
+    cond do
+      String.match?(phrase, ~r/^qu/) ->
+        [first_result, second_result, last_result] = Regex.split(~r{qu}, phrase, include_captures: true, parts: 2)
+        "#{last_result}#{second_result}ay"
+      Enum.member?(["a", "e", "i", "o", "u"], String.slice(phrase, 0..0)) -> 
+        phrase <> "ay"
+      Enum.member?(["a", "e", "i", "o", "u"], String.slice(phrase, 1..1)) ->
+        String.slice(phrase, 1..-1) <> String.slice(phrase, 0..0) <> "ay"
+      String.match?(phrase, ~r/qu/) ->
+        [first_result, second_result, last_result] = Regex.split(~r{qu}, phrase, include_captures: true, parts: 2)
+        "#{last_result}#{first_result}#{second_result}ay"
+      true -> 
+        [first_result, second_result, last_result] = Regex.split(~r{[aeiou]|([xy])(?=[^aeiou])}, phrase, include_captures: true, parts: 2)
+        "#{second_result}#{last_result}#{first_result}ay"
+    end
   end
 end
